9409b82e80c6b0c54dbe6f89ca9c65f993c4fa6a
jagruthigollu7799 <jagruthijagruthi178@gmail.com>
2026-09-21 19:12:27 +0530

    added python program

---
+++ areaOfRectangle.py
@@ -0,0 +1,4 @@
+length = float(input("Enter the length of the rectangle:"))
+width = float(input("Enter the width of the rectangle:"))
+Area_of_rectangle = length*width
+print(f"The area of the rectangle is {Area_of_rectangle}")
