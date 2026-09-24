0d27213e29ec5d68bc778c524175ba12ccfe1aed
jagruthigollu7799 <jagruthijagruthi178@gmail.com>
2026-09-24 20:54:17 +0530

    added python program

---
+++ Divisiblity.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a number:"))
+if a % 3 == 0:
+    print("This number is divisible by 3")
+elif a % 5 == 0:
+    print("This number is divisible by 5")
+    
+else:
+    print("This number is not divisible by both")
