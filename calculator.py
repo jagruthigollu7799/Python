17bcb807f2a38054661ec4cabec3ae600e403442
jagruthigollu7799 <jagruthijagruthi178@gmail.com>
2026-09-26 11:19:40 +0530

    added python program

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# python calculator
+operator = input("Enter an operator(+ - * /):")
+num1 = float(input("Enter the 1st number:"))
+num2 = float(input("Enter the 2nd number:"))
+
+if operator == "+":
+
+    print(num1 +num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+else :
+    print(f"This {operator} is invalid")
+  
